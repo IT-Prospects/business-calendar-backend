be260196f1964c2e2389d4b7dad82aa48a081b3f
putilove <gleb89047408502@gmail.com>
2023-12-14 20:45:01 +0400

    Удаление номера телефона

--- BusinessCalendar/Controllers/UserController.cs
+++ BusinessCalendar/Controllers/UserController.cs
@@ -39,9 +39,9 @@ namespace BusinessCalendar.Controllers
                 }
                 var item = MappingToDomainObject(itemDTO);
 
-                if (_userDAO.HasDuplicateByEmailOrPhoneNumber(item.Email, item.PhoneNumber))
+                if (_userDAO.HasDuplicateByEmail(item.Email))
                 {
-                    return BadRequest("This email or phone number already registered");
+                    return BadRequest("This email already registered");
                 }
 
                 item.PasswordHash = Convert.ToBase64String(SHA256.HashData(Encoding.UTF8.GetBytes(item.PasswordHash)));
@@ -129,9 +129,6 @@ namespace BusinessCalendar.Controllers
             if (string.IsNullOrWhiteSpace(item.LastName))
                 stringBuilder.AppendLine(string.Format(_requiredFieldErrorMessageTemplate, nameof(item.LastName)));
 
-            if (string.IsNullOrWhiteSpace(item.PhoneNumber))
-                stringBuilder.AppendLine(string.Format(_requiredFieldErrorMessageTemplate, nameof(item.PhoneNumber)));
-
             message = stringBuilder.ToString();
 
             return message == string.Empty;
@@ -159,7 +156,6 @@ namespace BusinessCalendar.Controllers
                         itemDTO.FirstName!,
                         itemDTO.LastName!,
                         itemDTO.Email!,
-                        itemDTO.PhoneNumber!,
                         itemDTO.Password!
                     );
         }
@@ -171,7 +167,6 @@ namespace BusinessCalendar.Controllers
                 FirstName = item.FirstName,
                 LastName = item.LastName,
                 Email = item.Email,
-                PhoneNumber = item.PhoneNumber
             };
         }
     }
---
+++ DAL/Migrations/20231214163822_RemovePhoneNumberProp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePhoneNumberProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "phonenumber",
+                table: "userset");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "phonenumber",
+                table: "userset",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
